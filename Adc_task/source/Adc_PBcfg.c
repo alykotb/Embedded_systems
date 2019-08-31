310b13f67096e131f5dc30dd3269afa9ccc335ed
alykotb <44653662+alykotb@users.noreply.github.com>
2019-08-31 17:11:25 +0200

    Add files via upload

---
+++ Adc_task/source/Adc_PBcfg.c
@@ -0,0 +1,15 @@
+/*
+ * Adc_Cfg.c
+ *
+ *  Created on: 31 Aug 2019
+ *      Author: Aly
+ */
+#include "includes/Adc.h"
+const Channel_To_Sequencer_mappingType channel_to_seq_map0[]=CH_Seq_Map0;
+const uint8 group0_channels[]={0,1};
+const uint8 group1_channels[]={2};
+const uint8 Grp_to_seq_map0[]=Group_seq_map0;
+uint8 Unit0_seq_counters[Num_of_seq_uint0]={0,0};
+const uint8 Unit0_seq_sizes[2]={2,1};
+
+
