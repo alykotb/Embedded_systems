1cbcdae5745fa1ec8e430a19c1c3a2064f288480
alykotb <44653662+alykotb@users.noreply.github.com>
2019-08-31 16:39:57 +0200

    Add files via upload

---
+++ Adc_task/main.c
@@ -0,0 +1,20 @@
+
+
+/**
+ * main.c
+ */
+
+#include "includes/Adc.h"
+
+
+;
+//#include"includes/Adc.h"
+int main(void)
+{
+    Adc_Init(&Adc_Config_Container);
+    Adc_DeInit();
+while(1){
+
+         }
+	return 0;
+}
